1499c59edee5b6afba0701eb5377ef379355655d
Arie <arnoudcommandeur@hotmail.com>
2020-10-19 00:11:29 +0200

    Updates

---
+++ commands.js
@@ -0,0 +1,36 @@
+// Init contract
+var instance = await Collectables.deployed();
+var accounts = await web3.eth.getAccounts();
+await instance.createNewEscaperoom(accounts[1], "The mask", "100000");
+await instance.mint(1, 2, {from: accounts[1], value:2000000});
+
+await instance.rewardVisitor(1, accounts[2], {from: accounts[1]} );
+await instance.rewardVisitorBatch(1, [accounts[2], accounts[3]], {from: accounts[1]} );
+await instance.balanceOf(accounts[2],1).then(result => result.toNumber());
+
+await instance.mintAdmin(1, 100);
+await instance.setPricePerCollectable(2000000);
+
+// Get contract balance
+await web3.eth.getBalance(instance.address).then(result => console.log(result));
+
+instance.EscaperoomAdmins(accounts[1]).then(result => result);
+
+instance.EscapeRoomCounter.call().then(function (res) {console.log(res)});
+instance.Escaperooms.call(1).then(function (res) {console.log(res)});
+
+
+
+// Testing
+instance.createNewEscaperoom(accounts[1], "The mask", "100000",{from: accounts[1]});
+
+
+
+instance.mint(accounts[0],5,100,"0x");
+
+instance.safeTransferFrom(accounts[0],accounts[1],1,5,"0x");
+
+instance.balanceOf(accounts[0],5).then(result => result.toNumber());
+
+instance.pause();
+instance.unpause();
--- contracts/Collectables.sol
+++ contracts/Collectables.sol
@@ -5,42 +5,123 @@ import "@openzeppelin/contracts/presets/ERC1155PresetMinterPauser.sol";
 
 contract Collectables is ERC1155PresetMinterPauser {
 
+    // Struct for escaperooms
     struct s_Escaperoom {
         uint _Id;
         string _Name;
         address _Admin;
         uint _TotalCollectables;
+        bool _enabled;
     }
 
-    //bytes32 public constant ESCAPEROOM_ADMIN_ROLE = keccak256("ESCAPEROOM_ADMIN_ROLE");
-    uint public EscapeRoomCounter;
+    // Escaperoom counter
+    uint public EscapeRoomCounter = 0;
+
+    // Conversionprice GWei per Collectable
+    uint public PricePerCollectable = 0 wei;
+
+    // Mapping between _id and escaperoom
     mapping(uint => s_Escaperoom) public Escaperooms;
+
+    // Mapping between address and the _id of the escaperoom
     mapping(address => uint) public EscaperoomAdmins;
 
+    // Events
+    event visitorRewarded(address indexed _visitor, uint indexed _id);
+    event pricePerCollectableUpdated(address indexed _admin, uint _pricePerCollectable);
+
+    // Constructor
     constructor() public ERC1155PresetMinterPauser("https://erc1155metadata.azurewebsites.net/api/token/{id}") {
-        // _mint(msg.sender, 2, 1, "");
-        // _mint(msg.sender, 0, 10**18, "");
+        // No code yet!
     }
 
-    function createEscaperoom(address _EscaperoomAdmin, string memory _Name, uint _TotalCollectables) public {
-        require(hasRole(DEFAULT_ADMIN_ROLE,msg.sender) == true, "Error: Calling address does not have DEFAULT_ADMIN_ROLE role");
-        require(_TotalCollectables > 0, "Error: TotalCollectables must be > 0");
-        require(_EscaperoomAdmin != address(0), "Error: _EscaperoomAdmin is invalid");
+    // Function to create a new escaperoom
+    // Only for Admins of the contract
+    // Escaperoom Admin will receive all tokens
+    function createNewEscaperoom(address _EscaperoomAdmin, string memory _Name, uint _FreeCollectables) public {
+        require(hasRole(DEFAULT_ADMIN_ROLE,msg.sender) == true, "ERROR: Calling address does not have DEFAULT_ADMIN_ROLE role");
+        require(_FreeCollectables > 0, "ERROR: TotalCollectables must be > 0");
+        require(_EscaperoomAdmin != address(0), "ERROR: _EscaperoomAdmin is invalid");
+        require(EscaperoomAdmins[_EscaperoomAdmin] == 0, "ERROR: User already has an escape room account");
+
+        EscapeRoomCounter += 1;
 
         EscaperoomAdmins[_EscaperoomAdmin] = EscapeRoomCounter;
-
-        //grantRole(ESCAPEROOM_ADMIN_ROLE, _EscaperoomAdmin);
 
         s_Escaperoom memory tmpEscaperoom;
         tmpEscaperoom._Id = EscapeRoomCounter;
         tmpEscaperoom._Name = _Name;
         tmpEscaperoom._Admin = _EscaperoomAdmin;
-        tmpEscaperoom._TotalCollectables = _TotalCollectables;
+        tmpEscaperoom._TotalCollectables = _FreeCollectables;
+        tmpEscaperoom._enabled = true;
 
         Escaperooms[EscapeRoomCounter] = tmpEscaperoom;
-        EscapeRoomCounter += 1;
+        _mint(_EscaperoomAdmin, EscapeRoomCounter, _FreeCollectables, "");
 
-        _mint(_EscaperoomAdmin, EscapeRoomCounter, _TotalCollectables, "");
     }
 
+    // Function to reqard a visitor with a collectable
+    // Only possible for EscaperoomAdmin
+    // Emit event after reward for visitor App
+    function rewardVisitor(uint _id, address _visitor) public {
+        require(EscaperoomAdmins[msg.sender] == _id, "ERROR: User is not an admin of the Escaperoom!");
+        require(balanceOf(_visitor, _id) == 0, "ERROR: User has collectable already");
+        safeTransferFrom(msg.sender,_visitor,_id,1,"0x");
+
+        // Emit event
+        emit visitorRewarded(_visitor, _id);
+    }
+
+    // Function to reqard a visitor with a collectable
+    // Only possible for EscaperoomAdmin
+    // Emit event after reward for visitor App
+    function rewardVisitorBatch(uint _id, address[] memory _visitors) public {
+        require(EscaperoomAdmins[msg.sender] == _id, "ERROR: User is not an admin of the Escaperoom!");
+
+        for (uint256 i = 0; i < _visitors.length; ++i) {
+            // Only reward visitor who haven't finished the room yet
+            if (balanceOf(_visitors[i], _id) == 0) {
+                safeTransferFrom(msg.sender,_visitors[i],_id,1,"0x");
+
+                // Emit event
+                emit visitorRewarded(_visitors[i], _id);
+            }
+        }
+    }
+
+    // Mint new collectables for escaperoom
+    // Only for Admins of the escaperoom
+    // Needs ether to buy collectibles
+    function mint(uint _id, uint _Amount) payable public {
+        require(EscaperoomAdmins[msg.sender] == _id, "ERROR: User is not an admin of the Escaperoom!");
+        require(_Amount > 0, "ERROR: _Amount must be > 0");
+        require(msg.value == (_Amount * PricePerCollectable), "ERROR: Too much/less ether sent");
+
+        address EscaperoomAdmin = Escaperooms[_id]._Admin;
+        _mint(EscaperoomAdmin, _id, _Amount, "");
+        Escaperooms[_id]._TotalCollectables += _Amount;
+    }
+
+    // Mint new collectables for escaperoom
+    // Only for contract admins
+    // Does not need Ether, free collectables!
+    function mintAdmin(uint _id, uint _Amount) public {
+        require(hasRole(DEFAULT_ADMIN_ROLE,msg.sender) == true, "ERROR: Calling address does not have DEFAULT_ADMIN_ROLE role");
+        require(_Amount > 0, "ERROR: _Amount must be > 0");
+
+        address EscaperoomAdmin = Escaperooms[_id]._Admin;
+        _mint(EscaperoomAdmin, _id, _Amount, "");
+        Escaperooms[_id]._TotalCollectables += _Amount;
+    }
+
+    // Set new price per collectable
+    // Only for contract admins
+    function setPricePerCollectable(uint _newPricePerCollectable) public {
+        require(hasRole(DEFAULT_ADMIN_ROLE,msg.sender) == true, "ERROR: Calling address does not have DEFAULT_ADMIN_ROLE role");
+        require(_newPricePerCollectable >= 0, "ERROR: _newPricePerCollectable must >= 0");
+
+        PricePerCollectable = _newPricePerCollectable;
+        emit pricePerCollectableUpdated(msg.sender, _newPricePerCollectable);
+
+    }
 }
